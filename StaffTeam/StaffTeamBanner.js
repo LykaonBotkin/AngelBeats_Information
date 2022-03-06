5043814558c89349fb6a946a08faa8183260fb30
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:04:17 -0800

    Create StaffTeamBanner.js

---
+++ StaffTeam/StaffTeamBanner.js
@@ -0,0 +1,10 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/943131242080116736\/947957847805796403\/staff_bg.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/943131242080116736\/947957847805796403\/staff_bg.png",
+        "width":3000,
+        "height":1000
+    },
+    "color":10631531,
+    "type":"rich"
+}
