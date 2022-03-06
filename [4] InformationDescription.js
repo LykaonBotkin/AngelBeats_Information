6527678b24c691a3dfc47022aa2cbc2d865a2612
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:46:26 -0800

    Create [4] InformationDescription.js

---
+++ [4] InformationDescription.js
@@ -0,0 +1,5 @@
+{
+    "color":16742751,
+    "type":"rich",
+    "description":"Below outlines all the information you would need to know about the server, such as ranking up or finding out more information relating to Angel Beats. If you need any more information or have questions, please don't hesitate to contact one of the <@&933085846549200897>! \n\nThank you! <:OtonashiHappy:933301209597607966>"
+}
