7313e86e19d9665738b710af73d558d12985424b
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:06:04 -0800

    Create StaffTeamAdministratorList.js

---
+++ StaffTeam/StaffTeamAdministratorList.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":"- <@264612287048843264>, the Student Council President.  Deals with server-related issues, bot development, and is the owner of the server.\n\n- <@615719995393179684>, the Acting President. Deals with developing bots and server-related issues.\n\n- <@206960269316980736>, the Head of Disciplinary Committee and Partnership Diplomat. Handling and investigating community matters. Also handles server-related issues.",
+    "title":"Your Faculty Committee"
+}
