4f7cb09aabfad3246f68df7b656897ad01b2b4e9
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:49:42 -0800

    Create [6] MediaAndLinksDiscordURLs.js

---
+++ [6] MediaAndLinksDiscordURLs.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":3948543,
+    "type":"rich",
+    "description":"https:\/\/discord.gg\/9cNAWKBaAe\n\nhttps:\/\/discord.gg\/angelbeats",
+    "title":"__Our Permanent and Vanity URLs:__"
+}
