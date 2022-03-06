6943efeab90036c651db50f38395142ec328fcbd
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:51:43 -0800

    Create [8] MediaAndLinksWatchSources.js

---
+++ [8] MediaAndLinksWatchSources.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":3948543,
+    "type":"rich",
+    "description":"<:CrunchyRoll:949247160996749322> **[Crunchyroll](https:\/\/www.crunchyroll.com\/angel-beats)**\n\n<:Funimation:949256063780286464> **[Funimation](https:\/\/www.funimation.com\/shows\/angel-beats)**\n\n<:Netflix:949246860814614528> **[Netflix](https:\/\/www.netflix.com\/gb\/title\/70201870)**",
+    "title":"__Watch On:__"
+}
