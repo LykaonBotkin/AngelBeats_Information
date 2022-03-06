1b637857fd16605d8699beda9ec3fee965b84c36
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:44:26 -0800

    Create InformationBanner.js

---
+++ InformationBanner.js
@@ -0,0 +1,10 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/943131242080116736\/948833545407852544\/info_ab.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/943131242080116736\/948833545407852544\/info_ab.png",
+        "width":3000,
+        "height":1000
+    },
+    "color":12675939,
+    "type":"rich"
+}
