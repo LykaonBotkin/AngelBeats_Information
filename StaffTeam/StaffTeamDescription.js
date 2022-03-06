c662b31bc92c4b2e3a17c02add1b3aee0ff2b74d
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:04:41 -0800

    Create StaffTeamDescription.js

---
+++ StaffTeam/StaffTeamDescription.js
@@ -0,0 +1,11 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":"The information below gives you an idea of who the staff team is and what roles they have."
+}
