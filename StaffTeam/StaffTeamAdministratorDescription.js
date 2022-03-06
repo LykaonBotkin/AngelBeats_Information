7a75b0347492fc46acf2ceb02f7794fea18b8141
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:05:40 -0800

    Create StaffTeamAdministratorDescription.js

---
+++ StaffTeam/StaffTeamAdministratorDescription.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":":small_blue_diamond: <@&935297556223762493>\nThe Faculty Committee, also known as the \"administrators\", are responsible for maintaining and developing the server itself, such as bots, roles, channels, permissions, stickers, emotes, etc. They also make decisions relating to the future of the server and the current well-being.",
+    "title":"__The Faculty Committee (Administrators)__"
+}
