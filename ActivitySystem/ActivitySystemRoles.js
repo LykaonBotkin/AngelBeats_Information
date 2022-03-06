31f1195126154047aa48fde050791209fdd06649
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:01:51 -0800

    Create ActivitySystemRoles.js

---
+++ ActivitySystem/ActivitySystemRoles.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10384994,
+    "type":"rich",
+    "description":":small_blue_diamond: <@&933187488053334022>\nThe bottom of the totem pole. Typically, brand new members and less active people have this role. However, this is nothing to be ashamed of as everyone here has been in your shoes. Treat this as your entrance exam into the server and give it all you got!\n\n:small_blue_diamond: <@&933187568944685076>\nCongratulations! :confetti_ball: You have engaged in conversations or have participated in the server. You are now recognized as an active member of our community\u2019s family and have earned your place here.\n\n:small_blue_diamond: <@&933187649143980064>\nAmazing! You\u2019re near the top of the food chain. You are now acknowledged as one of the most active members of the community due to your time spent here active and your positive impact on the student body. Thank you for being a role model and someone to look up to! :partying_face:",
+    "title":"__Ranks:__"
+}
