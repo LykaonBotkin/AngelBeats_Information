e1912bb05f9af46f18c7e33eda2660ac8e12402a
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:15:21 -0800

    Create ChannelsBanner.js

---
+++ Channels/ChannelsBanner.js
@@ -0,0 +1,10 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/943131242080116736\/948845952654000148\/channels.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/943131242080116736\/948845952654000148\/channels.png",
+        "width":3000,
+        "height":1000
+    },
+    "color":16773826,
+    "type":"rich"
+}
