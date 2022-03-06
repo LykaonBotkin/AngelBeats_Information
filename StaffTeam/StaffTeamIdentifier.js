3bbdb4dfc4f3f2b21d622ea5908a55e87a1e006a
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:05:04 -0800

    Create StaffTeamIdentifier.js

---
+++ StaffTeam/StaffTeamIdentifier.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":":small_blue_diamond: <@&933085846549200897>\nA team of students that encompass the entire staff of the server. Composing of the Faculty Committee and Disciplinary Committee. This role is given to anyone with any form of power. Overall, they keep the entire server running smoothly.",
+    "title":"__Staff Identifier:__"
+}
