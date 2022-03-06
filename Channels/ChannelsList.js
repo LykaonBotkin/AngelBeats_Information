629c81ab6155490dc7dcc2f95195d25084266fc5
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:16:06 -0800

    Create ChannelsList.js

---
+++ Channels/ChannelsList.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":16773826,
+    "type":"rich",
+    "description":":small_blue_diamond: <#933083666912014419>\nThis channel is for everyday, casual talk. Just don\u2019t cause too much drama, and don\u2019t needlessly use bot commands.\n\n:small_blue_diamond: <#933134770408398898>\nA channel for all anime and manga-related discussions. Except for Angel Beats, we have a special channel for that.\n\n:small_blue_diamond: <#933134772019007539>\nChannel for discussing gaming. So get your gaming face on and have some fun!\n\n:small_blue_diamond: <#933131632284696667>\nA channel for all anime and manga-related images. Slightly NSFW images are allowed here, but images depicting nudity or deemed too lewd by a moderator should be kept to <#933114483411386459>.\n\n:small_blue_diamond: <#933131642661396481>\nFor cosplay, fanart, and other community-created works.\n\n:small_blue_diamond: <#933131629965246515> \nWelcome to osu! Click the circles! If you like to play osu! go pick up the role from <#933123974630608936> to gain access for this channel.\n\n:small_blue_diamond: <#933134772438437888>\nDoesn\u2019t quite fit in anywhere else or don\u2019t want to interrupt a conversation in <#933083666912014419>? Post it here. It\u2019s mainly for memes and shitposting though.\n\n:small_blue_diamond: <#933134770936885309>\nA channel for discussing IRL topics. This is not a venting channel, but you are allowed to vent as long as it obeys the server rules. Not for discussing heavy personal issues so keep them off-server or in DMs with someone you trust.\n\n:small_blue_diamond: <#933134767719870565>\nThis channel is not for casual talking or the like. Matters of emergency, political discussions (which are only discussed here), or concerns that the student council can deal with may be placed here.\n\n:small_blue_diamond: <#933127290827710534>\nThis is where the NPCs lurk. Keep bot spam to this channel.",
+    "title":"__Channels:__"
+}
