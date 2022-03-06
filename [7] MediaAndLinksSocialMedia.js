42ea2c454ae854254b653350734736fb999c0bee
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:50:24 -0800

    Create [7] MediaAndLinksSocialMedia.js

---
+++ [7] MediaAndLinksSocialMedia.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":3948543,
+    "type":"rich",
+    "description":"<:Reddit:948120832658530354> **[r\/AngelBeats](https:\/\/www.reddit.com\/r\/AngelBeats\/)**\n\n<:AniList:948118142197059605> **[AniList](https:\/\/anilist.co\/anime\/6547\/Angel-Beats\/)**\n\n<:MAL:948118449673093160> **[MyAnimeList](https:\/\/myanimelist.net\/anime\/6547\/Angel_Beats)**\n\n<:Fandom:948122546216263701> **[Angel Beats! Wiki](https:\/\/angelbeats.fandom.com\/wiki\/Angel_Beats_Wiki)**\n\n<:ANN:948121317901750272> **[Anime News Network](https:\/\/www.animenewsnetwork.com\/encyclopedia\/anime.php?id=10885)**\n\n<:W:949263101843361842> **[Wikipedia](https:\/\/en.wikipedia.org\/wiki\/Angel_Beats!)**\n\n<:Kitsu:950101379509268550> **[Kitsu](https:\/\/kitsu.io\/anime\/angel-beats)**\n\n<:SSSModified:949256063323082782> **[Official Website (JP)](http:\/\/www.angelbeats.jp\/)**",
+    "title":"__Social Media Links:__"
+}
