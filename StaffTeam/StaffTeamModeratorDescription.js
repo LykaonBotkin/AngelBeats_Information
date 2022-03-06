7c6a2ae3dfb478b3574b497842ca92903b6e2e39
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:08:41 -0800

    Create StaffTeamModeratorDescription.js

---
+++ StaffTeam/StaffTeamModeratorDescription.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":":small_blue_diamond: <@&935297824403357706>\nThe Disciplinary Committee, also known as the \"moderators\", focuses primarily on the rest of the student body and is responsible for relations with the other students. Has the authority to enforce the rules.",
+    "title":"__The Disciplinary Committee (Moderators)__"
+}
