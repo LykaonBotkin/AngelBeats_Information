bea3b6b15db550157bbac47d818d2274f59e9d9a
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:09:53 -0800

    Create StaffTeamModeratorList.js

---
+++ StaffTeam/StaffTeamModeratorList.js
@@ -0,0 +1,12 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":10631531,
+    "type":"rich",
+    "description":"- <@229258917732024321>\n\n- <@256789973749989376>\n\n- <@859676332757090354>\n\n- <@345294695544651776>\n\n- <@283741910340730880> (Pending Implementation)\n\n- <@514333854543380482> (Pending Implementation)\n\n- <@97122523086340096> (Pending Implementation)\n\n- <@315203962598981632> (Pending Implementation)",
+    "title":"Your Disciplinary Committee"
+}
