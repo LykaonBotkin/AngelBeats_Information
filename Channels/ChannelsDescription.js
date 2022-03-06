f188a47d36321e749dcb20df0e97e65e34f2d3c1
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 15:15:44 -0800

    Create ChannelsDescription.js

---
+++ Channels/ChannelsDescription.js
@@ -0,0 +1,11 @@
+{
+    "image":{
+        "url":"https:\/\/cdn.discordapp.com\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "proxy_url":"https:\/\/media.discordapp.net\/attachments\/933131629487079435\/948844489051607040\/bottom_bar.png",
+        "width":800,
+        "height":6
+    },
+    "color":16773826,
+    "type":"rich",
+    "description":"Here are some short descriptions of some of the channels that the server offers."
+}
